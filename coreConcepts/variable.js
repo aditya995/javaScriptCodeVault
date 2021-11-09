b017fe7dc78e5fc0157b88f5b0a9f2713c1a21e0
aditya995 <39437959+aditya995@users.noreply.github.com>
2021-11-09 19:12:02 +0600

    Variables in javaScript

--- coreConcepts/variable.js
+++ coreConcepts/variable.js
@@ -2,6 +2,42 @@
 node variable
 */
 
-var car = 4;
-console.log('🍕🍕🍿');
+console.log('Finally 🍕🍕🍿');
+
+
+/** Hoisting with var declaration */
+car = 12;   //  Value can be asigned before declaration!
+var car = 40;    
 console.log(car);
+
+
+/** Const Keyword */
+const apple = 45;   // const variables must be Initialized
+console.log(apple);
+
+
+/** Data Types */
+console.log("\nData Types: \n");
+data;
+console.log(typeof(data));  //undefined
+data = 8;
+console.log(typeof(data));  //number
+var data = 'StringData';
+console.log(typeof(data));  //string
+data = null;
+console.log(typeof(data));  //object
+data = true;
+console.log(typeof(data));  //boolean
+
+
+/** scoping */
+var a = null;
+//var a initialized before-hand
+console.log(typeof(a)); //object
+
+a = 8;  //  This value-assign will change the variable's type dynamically!
+console.log(typeof(a)); //number
+
+var a;  
+console.log(typeof(a)); // number
+//Type is still number ! not undefined type!
